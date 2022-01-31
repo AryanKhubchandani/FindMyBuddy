cae6017ef541ebd8100e754dd7d63152ac8db051
Aryan <aryan.khubchandani@gmail.com>
2022-01-31 18:52:04 +0530

    feat: initialize login page

---
+++ lib/loginpage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("FindMyBuddy"),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {},
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              color: Colors.blue,
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: const Text(
+              "Sign In with Google",
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
