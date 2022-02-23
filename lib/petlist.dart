1b97aa4db0dc2a4ce834ea5c7550a87b301a9e39
Aryan <aryan.khubchandani@gmail.com>
2022-02-23 17:44:13 +0530

    chore: add pet list

---
+++ lib/petlist.dart
@@ -0,0 +1,56 @@
+List<Map> pets = [
+  {
+    "id": "1",
+    "name": "Snoopy",
+    "imagePath": "assets/images/img1.jpg",
+    "breed": "Beagle",
+    "gender": "male",
+    "age": "9",
+    "number": "9999401575"
+  },
+  {
+    "id": "2",
+    "name": "Candy",
+    "imagePath": "assets/images/img2.jpeg",
+    "breed": "Lhasa Apso",
+    "gender": "female",
+    "age": "2",
+    "number": "9076667788"
+  },
+  {
+    "id": "3",
+    "name": "Ted",
+    "imagePath": "assets/images/img3.jpeg",
+    "breed": "Shitzu",
+    "gender": "male",
+    "age": "1",
+    "number": "9984455671"
+  },
+  {
+    "id": "4",
+    "name": "Casper",
+    "imagePath": "assets/images/img4.jpeg",
+    "breed": "Labrador",
+    "gender": "male",
+    "age": "4",
+    "number": "9184523065"
+  },
+  {
+    "id": "5",
+    "name": "Tweetu",
+    "imagePath": "assets/images/img5.jpeg",
+    "breed": "Rose-ringed Parakeet",
+    "gender": "male",
+    "age": "1",
+    "number": "8564823092"
+  },
+  {
+    "id": "6",
+    "name": "Pooh",
+    "imagePath": "assets/images/img6.jpg",
+    "breed": "Persian Cat",
+    "gender": "female",
+    "age": "2",
+    "number": "9899657765"
+  },
+];
