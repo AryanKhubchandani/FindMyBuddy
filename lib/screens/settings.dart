adbf14d008c1baae8db83665de164c6bac276496
Aryan <aryan.khubchandani@gmail.com>
2022-02-23 17:45:10 +0530

    feat: add settings page

---
+++ lib/screens/settings.dart
@@ -0,0 +1,163 @@
+import 'dart:io';
+
+import 'package:find_my_buddy/screens/loginpage.dart';
+import 'package:find_my_buddy/services/sharedprefs.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path/path.dart' as path;
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  TextEditingController nameController = TextEditingController();
+  String myName = '', myImage = '', myNumber = '';
+  getInfo() async {
+    myName = (await SharedPrefs().getDisplayName())!;
+    myImage = (await SharedPrefs().getUserProfileUrl())!;
+    myNumber = (await SharedPrefs().getPhoneNumber())!;
+  }
+
+  String url = '';
+
+  FirebaseStorage storage = FirebaseStorage.instance;
+
+  Future<void> _upload() async {
+    final picker = ImagePicker();
+
+    PickedFile? pickedImage;
+    try {
+      pickedImage = await picker.getImage(
+        source: ImageSource.gallery,
+      );
+
+      final String fileName = path.basename(pickedImage!.path);
+      File imageFile = File(pickedImage.path);
+
+      try {
+        Reference ref = storage.ref().child(myNumber);
+
+        UploadTask uploadTask = ref.putFile(imageFile);
+        uploadTask.whenComplete(() async {
+          url = await ref.getDownloadURL();
+          updateImg();
+          setState(() {});
+        }).catchError((onError) {
+          print(onError);
+        });
+        setState(() {});
+      } on FirebaseException catch (error) {
+        print(error);
+      }
+    } catch (err) {
+      print(err);
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getInfo();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Expanded(
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 50.0),
+                    child: InkWell(
+                      onTap: () {
+                        _upload();
+                      },
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(myImage),
+                        radius: 50,
+                      ),
+                    ),
+                  ),
+                  const Text(
+                    "Name",
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: myName,
+                      ),
+                      onSubmitted: (value) {
+                        if (value.isNotEmpty) {
+                          updateName();
+                          setState(() {});
+                        }
+                      }),
+                  const SizedBox(height: 100.0),
+                  const Text(
+                    "Phone Number",
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextField(
+                    enabled: false,
+                    decoration: InputDecoration(
+                      hintText: myNumber,
+                    ),
+                  ),
+                  const SizedBox(height: 80.0),
+                  ElevatedButton(
+                      onPressed: () async {
+                        FirebaseAuth.instance.signOut();
+                        SharedPreferences prefs =
+                            await SharedPreferences.getInstance();
+                        prefs.clear();
+                        Navigator.of(context).popUntil((route) => false);
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (c) => const LoginPage()));
+                      },
+                      child: const Text("LOG OUT")),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void updateImg() async {
+    await SharedPrefs().saveUserProfileUrl(url);
+
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .update({'imgUrl': SharedPrefs().getUserProfileUrl()});
+  }
+
+  void updateName() async {
+    await SharedPrefs().saveDisplayName(nameController.text);
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .update({'name': nameController.text});
+  }
+}
